cf963cbde88030090ca018df6268c38f22bdef7f
emuaki <sugiura@emuaki.com>
2011-10-18 10:58:12 +0000

    add ejs files

--- web.js
+++ web.js
@@ -5,6 +5,17 @@ app.listen(port);
 
 app.use(express.static(__dirname + '/public'));
 
+var ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+    // テンプレートに渡す変数
+	var mes = "<p>hello world!</p>";
+	res.render('index.html', {locals:{mes:mes}});
+});
+
 var priceSettings = require('price_settings');
 var priceSimulator = require('price_simulator').createPriceSimulator(io, priceSettings);
 priceSimulator.start();
