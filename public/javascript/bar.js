d7caef703e2cd0b51579e817795c3645100814e7
dev <dev@emuaki.com>
2011-11-25 11:10:38 +0000

    modify

--- public/javascript/bar.js
+++ public/javascript/bar.js
@@ -1,4 +1,66 @@
+
 $.jqplot.config.enablePlugins = true;
+
+var TickPanel = function(option){
+    this.initialize(option);
+};
+
+TickPanel.prototype = {
+    
+    initialDataUrl : "./barData?currencyPair=",
+    
+    jqplotOption : {
+        axesDefaults: {
+            showMark : false,
+            fill:true,
+            fillToZero: true
+        },
+        axes: {
+            xaxis: {
+                renderer:$.jqplot.DateAxisRenderer,
+                tickOptions:{formatString:'%M:%S'}
+            },
+            yaxis: {
+                tickOptions:{formatString:'%.2f'}
+            }
+        }
+    },
+    
+    initialize : function(option){
+        this.currencyPair = option.currencyPair;
+        this.elementId = option.elementId;
+        this.data = this.getInitialData();
+    },
+    
+    getInitialData : function(){
+        var ret = null;
+        $.ajax({
+            async: false,
+            url: this.initialDataUrl + this.currencyPair,
+            dataType:"json",
+            success: function(data) {
+                ret = data;
+            }
+        });
+        return [ret];
+    },
+    
+    add : function(){
+        
+    },
+    
+    adjust : function(){
+        
+    },
+    
+    draw : function(){
+        $.jqplot('bar', this.data, this.jqplotOption);
+    }
+};
+
+
+    
+
 
 function createBar(currencyPair){
 
@@ -17,6 +79,11 @@ function createBar(currencyPair){
 
     plot1 = $.jqplot('bar', "./barData?currencyPair=" + currencyPair,{
       dataRenderer: ajaxDataRenderer,
+      axesDefaults: {
+        showMark : false,
+        fill:true,
+        fillToZero: true
+      },
       axes: {
           xaxis: {
               renderer:$.jqplot.DateAxisRenderer,
