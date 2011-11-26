76ae78a85150e87bfcd87a69c13013fdfe5a0dbe
dev <dev@emuaki.com>
2011-11-26 14:09:52 +0000

    modify

--- public/javascript/bar.js
+++ public/javascript/bar.js
@@ -1,3 +1,48 @@
+var BarPublisher = function(socket, option){
+    this.initialize(socket, option);
+};
+
+BarPublisher.prototype = {
+    
+    listenerMap : {},
+        
+    initialize : function(socket, option) {
+		this.socket = socket;
+        this.option = option;
+        
+        var self = this;
+        this.socket.on('bar', function (bar) {
+            self.onBar(bar);
+        });
+	},
+
+	onBar: function(bar) {
+        var currencyPair = price.currencyPair;
+        var listeners = this.listenerMap[currencyPair];
+        for(var j in listeners){
+            listeners[j].onBar(bar);
+        }
+	},
+    
+    addBarListener: function(currencyPair, listener){
+        if(undefined === this.listenerMap[currencyPair]){
+            this.listenerMap[currencyPair] = [];
+        }
+        this.listenerMap[currencyPair].push(listener);
+    },
+    
+    removeBarListener : function(currencyPair, listener){
+        var listeners = this.listenerMap[currencyPair];
+        for(var i in listeners){
+            if(listeners[i] === listener){
+                delete listeners[i];
+                break;
+            }
+        }
+    }
+};
+
+
 
 $.jqplot.config.enablePlugins = true;
 
@@ -21,7 +66,7 @@ TickPanel.prototype = {
                 renderer:$.jqplot.DateAxisRenderer,
                 tickOptions:{formatString:'%H:%M:%S'},
                 pad : 0,
-                tickInterval: 20
+                tickInterval: 40
             },
             yaxis: {
                 tickOptions:{formatString:'%.2f'}
