86dd7fe4dfd1303c92967d69f7070b0b76149002
dev <dev@emuaki.com>
2011-12-07 08:47:12 +0000

    modify

--- test/bar/date_test.js
+++ test/bar/date_test.js
@@ -4,14 +4,22 @@ var DateUtil = require('date').DateUtil;
 
 module.exports = {
     
-    "test plus9hour" : function(){
-        var date = new Date(2011, 12, 7, 15, 54, 30);
-        assert.equal(date.toString(), "Sat Jan 07 2012 15:54:30 GMT+0000 (GMT)");
+    "test addHour" : function(){
+        var date = new Date(2011, 11, 7, 15, 54, 30);
+        assert.equal(date.toString(), "Wed Dec 07 2011 15:54:30 GMT+0000 (GMT)");
         
-        var plus9hour = DateUtil.plus9hour(date);
-        assert.equal(plus9hour.toString(), "Sun Jan 08 2012 00:54:30 GMT+0000 (GMT)");
+        var plus1hour = DateUtil.addHour(date, 1);
+        assert.equal(plus1hour.toString(), "Wed Dec 07 2011 16:54:30 GMT+0000 (GMT)");
+
+        var plus9hour = DateUtil.addHour(date, 9);
+        assert.equal(plus9hour.toString(), "Thu Dec 08 2011 00:54:30 GMT+0000 (GMT)");
+        
     },
     
+    "test format" : function(){
+        var date = new Date(2011, 11, 7, 15, 54, 30);
+        assert.equal(DateUtil.format(date), "2011/12/07 15:54:30");
+    },
     
     "test currentDateAsString" : function() {
         var date = new Date(2011, 12, 7, 15, 54, 30);
