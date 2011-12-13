2290e4ed3ecad94beefc0e73e9f6c4c8be8fdc3a
dev <dev@emuaki.com>
2011-12-13 23:38:45 +0000

    modify

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,22 +1,41 @@
-var ChatSender = function(socket, args){
-    this.initialize(socket, args);
+var ChatPanel = function(option){
+    this.initialize(option);   
 };
 
-ChatSender.prototype = {
-    initialize: function(socket, args){
-        this.socket = socket;
-        this.args = args;
-    }
-};
-
-
-var ChatReceiver = function(socket, args){
-    this.initialize(socket, args);
-};
-ChatReceiver.prototype = {
+ChatPanel.prototype = {
     
-    initialize: function(socket, args){
-        this.socket = socket;
-        this.args = args;
+    initialize : function(option){
+        this.socket = option.socket;
+        this.sendButton = $("#" + option.sendButtonId);
+        this.messageArea = $("#" + option.messageAreaId);
+    },
+    
+    setupListener : function(){
+        var self = this;
+        this.sendButton.click(function(){
+            self.send();
+        });
+        
+        this.socket.on('chatMessage', function(data){
+            self.onMessageReceive(data);   
+        });
+    },
+    
+    send : function(){
+        var message = this.messageArea.val();
+        this.socket.emit("chatMessage", {message : message});
+        $.mobile.changePage("#chat", { transition: "slidedown"} );
+        this.messageArea.val("");
+    },
+    
+    onMessageReceive : function(data){
+        $("#notificationMessage").remove();
+        var list = jQuery("<li>" + data.message + "</li>");
+        list.addClass("ui-li");
+        list.addClass("ui-li-static");
+        list.addClass("ui-body-c");
+        $("#chatMessages").append(list);
     }
+    
 };
+
