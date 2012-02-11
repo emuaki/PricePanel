bbeb94d188c267e73efb15c26afa11d90f6b45fd
dev <dev@emuaki.com>
2012-02-11 05:46:30 +0000

    "modify"

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
-require.paths.push('app');
-console.log(require.paths);
+//require.paths.push('app');
+//console.log(require.paths);
+process.env['NODE_PATH'] = __dirname + '/app';  
+require("module")._initPaths();  
 require('bootstrap');
